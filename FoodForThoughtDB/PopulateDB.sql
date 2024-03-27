a513a530b90c55715547d0f20ed38c8cc7582fb1
Kevon3 <143442942+Kevon3@users.noreply.github.com>
2024-03-27 15:35:45 -0500

    Added cuisines

--- FoodForThoughtDB/PopulateDB.sql
+++ FoodForThoughtDB/PopulateDB.sql
@@ -27,4 +27,33 @@ INSERT [dbo].[Allergies] ([AllergyId], [Name]) VALUES (8, N'Wheat')
 GO
 INSERT [dbo].[Allergies] ([AllergyId], [Name]) VALUES (9, N'Sesame')
 GO
+INSERT [dbo].[Cuisine] ([CuisineId], [Cuisine]) VALUES (1, N'American')
+GO
+INSERT [dbo].[Cuisine] ([CuisineId], [Cuisine]) VALUES (2, N'Chinese')
+GO
+INSERT [dbo].[Cuisine] ([CuisineId], [Cuisine]) VALUES (3, N'Greek')
+GO
+INSERT [dbo].[Cuisine] ([CuisineId], [Cuisine]) VALUES (4, N'Indian')
+GO
+INSERT [dbo].[Cuisine] ([CuisineId], [Cuisine]) VALUES (5, N'Italian')
+GO
+INSERT [dbo].[Cuisine] ([CuisineId], [Cuisine]) VALUES (6, N'Japanese')
+GO
+INSERT [dbo].[Cuisine] ([CuisineId], [Cuisine]) VALUES (7, N'Korean')
+GO
+INSERT [dbo].[Cuisine] ([CuisineId], [Cuisine]) VALUES (8, N'Mexican')
+GO
+INSERT [dbo].[Cuisine] ([CuisineId], [Cuisine]) VALUES (9, N'Thai')
+GO
+INSERT [dbo].[Cuisine] ([CuisineId], [Cuisine]) VALUES (10, N'Vietnamese')
+GO
+INSERT [dbo].[Cuisine] ([CuisineId], [Cuisine]) VALUES (11, N'Soul')
+GO
+INSERT [dbo].[Cuisine] ([CuisineId], [Cuisine]) VALUES (12, N'Creole')
+GO
+INSERT [dbo].[Cuisine] ([CuisineId], [Cuisine]) VALUES (13, N'French')
+GO
+INSERT [dbo].[Cuisine] ([CuisineId], [Cuisine]) VALUES (14, N'Other')
+GO
+
 
