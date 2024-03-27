b8b21c6fe4ccb8e3abae70d62041206ce39daaef
Kevon3 <143442942+Kevon3@users.noreply.github.com>
2024-03-27 15:17:24 -0500

    Added Allergies to original

--- FoodForThoughtDB/PopulateDB.sql
+++ FoodForThoughtDB/PopulateDB.sql
@@ -9,3 +9,22 @@ Post-Deployment Script Template
                SELECT * FROM [$(TableName)]					
 --------------------------------------------------------------------------------------
 */
+INSERT [dbo].[Allergies] ([AllergyId], [Name]) VALUES (1, N'Eggs')
+GO
+INSERT [dbo].[Allergies] ([AllergyId], [Name]) VALUES (2, N'Milk')
+GO
+INSERT [dbo].[Allergies] ([AllergyId], [Name]) VALUES (3, N'Soy')
+GO
+INSERT [dbo].[Allergies] ([AllergyId], [Name]) VALUES (4, N'Gluten')
+GO
+INSERT [dbo].[Allergies] ([AllergyId], [Name]) VALUES (5, N'Shellfish')
+GO
+INSERT [dbo].[Allergies] ([AllergyId], [Name]) VALUES (6, N'Nuts')
+GO
+INSERT [dbo].[Allergies] ([AllergyId], [Name]) VALUES (7, N'Fish')
+GO
+INSERT [dbo].[Allergies] ([AllergyId], [Name]) VALUES (8, N'Wheat')
+GO
+INSERT [dbo].[Allergies] ([AllergyId], [Name]) VALUES (9, N'Sesame')
+GO
+
