3984160ca0bbf1754a1d90ca5cfea14b3e02a253
Kevon3 <143442942+Kevon3@users.noreply.github.com>
2024-03-27 15:13:11 -0500

    Added Allergies data

---
+++ FoodForThoughtDB/PopulateDB_1.sql
@@ -0,0 +1,30 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+INSERT [dbo].[Allergies] ([AllergyId], [Name]) VALUES (1, N'Eggs')
+GO
+INSERT [dbo].[Allergies] ([AllergyId], [Name]) VALUES (2, N'Milk')
+GO
+INSERT [dbo].[Allergies] ([AllergyId], [Name]) VALUES (3, N'Soy')
+GO
+INSERT [dbo].[Allergies] ([AllergyId], [Name]) VALUES (4, N'Gluten')
+GO
+INSERT [dbo].[Allergies] ([AllergyId], [Name]) VALUES (5, N'Shellfish')
+GO
+INSERT [dbo].[Allergies] ([AllergyId], [Name]) VALUES (6, N'Nuts')
+GO
+INSERT [dbo].[Allergies] ([AllergyId], [Name]) VALUES (7, N'Fish')
+GO
+INSERT [dbo].[Allergies] ([AllergyId], [Name]) VALUES (8, N'Wheat')
+GO
+INSERT [dbo].[Allergies] ([AllergyId], [Name]) VALUES (9, N'Sesame')
+GO
+
