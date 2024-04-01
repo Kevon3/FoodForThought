3d351b8bcd52632ecb1fc076c0dea4883e2049ca
Kevon3 <143442942+Kevon3@users.noreply.github.com>
2024-04-01 15:07:52 -0500

    Conflict completes

--- FoodForThoughtDB/dbo/Tables/UserRecipe.sql
+++ FoodForThoughtDB/dbo/Tables/UserRecipe.sql
@@ -4,6 +4,8 @@
     [RecipeId]     INT NOT NULL,
     CONSTRAINT [PK_UserRecipe] PRIMARY KEY CLUSTERED ([UserRecipeId] ASC),
     CONSTRAINT [FK_UserRecipe_Recipe] FOREIGN KEY ([RecipeId]) REFERENCES [dbo].[Recipe] ([RecipeId]),
-    CONSTRAINT [FK_UserRecipe_User] FOREIGN KEY ([UserId]) REFERENCES [dbo].[User] ([UserId])
+    CONSTRAINT [FK_UserRecipe_User] FOREIGN KEY ([UserId]) REFERENCES [dbo].[Person] ([UserId])
 );
+
+
 
