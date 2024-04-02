5700cff22a57b462d9129c8be05bf7608b28dd94
Kevon3 <143442942+Kevon3@users.noreply.github.com>
2024-04-01 22:32:19 -0500

    password to 200

--- FoodForThoughtDB/dbo/Tables/Person.sql
+++ FoodForThoughtDB/dbo/Tables/Person.sql
@@ -4,10 +4,12 @@
     [LastName]  NVARCHAR (50)  NOT NULL,
     [Email]     NVARCHAR (50)  NOT NULL,
     [Username]  NVARCHAR (20)  NOT NULL,
-    [Password]  NVARCHAR (20)  NOT NULL,
+    [Password]  NVARCHAR (200) NOT NULL,
     [url]       NVARCHAR (200) NOT NULL,
     [AllergyId] INT            NOT NULL,
     CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED ([UserId] ASC),
     CONSTRAINT [FK_User_Allergies] FOREIGN KEY ([AllergyId]) REFERENCES [dbo].[Allergies] ([AllergyId])
 );
+
+
 
