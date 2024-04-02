f276852aeb372e2ebfda7f4670a75fb331ed2b98
Kevon3 <143442942+Kevon3@users.noreply.github.com>
2024-04-01 23:17:50 -0500

    url allow null

--- FoodForThoughtDB/dbo/Tables/Person.sql
+++ FoodForThoughtDB/dbo/Tables/Person.sql
@@ -5,11 +5,13 @@
     [Email]     NVARCHAR (50)  NOT NULL,
     [Username]  NVARCHAR (20)  NOT NULL,
     [Password]  NVARCHAR (200) NOT NULL,
-    [url]       NVARCHAR (200) NOT NULL,
+    [url]       NVARCHAR (200) NULL,
     [AllergyId] INT            NULL,
     CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED ([UserId] ASC),
     CONSTRAINT [FK_User_Allergies] FOREIGN KEY ([AllergyId]) REFERENCES [dbo].[Allergies] ([AllergyId])
 );
+
+
 
 
 
