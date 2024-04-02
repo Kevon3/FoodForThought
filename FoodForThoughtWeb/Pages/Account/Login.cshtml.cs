4609570caec800a323399fc0261b06df7700433a
jaraePink <143231241+jaraePink@users.noreply.github.com>
2024-04-02 01:08:42 -0500

    okay okay last last commit :D

--- FoodForThoughtWeb/Pages/Account/Login.cshtml.cs
+++ FoodForThoughtWeb/Pages/Account/Login.cshtml.cs
@@ -33,7 +33,7 @@ namespace FoodForThoughtWeb.Pages.Account
                         string passwordHash = reader.GetString(0);
                         if (SecurityHelper.VerifyPassword(LoginUser.Password, passwordHash))
                         {
-                            return RedirectToPage("Register");
+                            return RedirectToPage("Profile");
                         }
                         else
                         {
@@ -48,7 +48,7 @@ namespace FoodForThoughtWeb.Pages.Account
 
                 }
                 conn.Close();
-                return RedirectToPage("Register");
+                return RedirectToPage("Profile");
 
             }
             else
