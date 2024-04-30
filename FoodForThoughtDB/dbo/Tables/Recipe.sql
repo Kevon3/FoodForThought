42f51b45172ab669513b53d600996a84ea6d1c61
Kevon3 <143442942+Kevon3@users.noreply.github.com>
2024-04-30 18:54:56 -0500

    url size updated

--- FoodForThoughtDB/dbo/Tables/Recipe.sql
+++ FoodForThoughtDB/dbo/Tables/Recipe.sql
@@ -2,13 +2,15 @@
     [RecipeId]    INT             IDENTITY (1, 1) NOT NULL,
     [DishName]    NVARCHAR (100)  NOT NULL,
     [Rating]      INT             NOT NULL,
-    [url]         NVARCHAR (200)  NULL,
+    [url]         NVARCHAR (1000) NULL,
     [CuisineId]   INT             NOT NULL,
     [Ingredients] NVARCHAR (4000) NOT NULL,
     [Steps]       NVARCHAR (4000) NOT NULL,
     CONSTRAINT [PK_Recipe] PRIMARY KEY CLUSTERED ([RecipeId] ASC),
     CONSTRAINT [FK_Recipe_Cuisine] FOREIGN KEY ([CuisineId]) REFERENCES [dbo].[Cuisine] ([CuisineId])
 );
+
+
 
 
 
