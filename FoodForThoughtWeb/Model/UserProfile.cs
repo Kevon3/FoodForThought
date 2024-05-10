6847f62aef363636769e482e1fcd97e3f6568491
jaraePink <143231241+jaraePink@users.noreply.github.com>
2024-05-10 16:30:19 -0500

    slight changes

--- FoodForThoughtWeb/Model/UserProfile.cs
+++ FoodForThoughtWeb/Model/UserProfile.cs
@@ -4,28 +4,26 @@ namespace FoodForThoughtWeb.Model
 {
     public class UserProfile
     {
-        public int UserId { get; set; }
+       // public int UserId { get; set; }
         //[Required]
 
-        //[Required(ErrorMessage = "The Username field is required.")]
+        [Required(ErrorMessage = "The Username field is required.")]
         //[Display(Name = "Username")]
         public string Username { get; set; }
 
-        //[Required(ErrorMessage = "The First Name field is required.")]
+        [Required(ErrorMessage = "The First Name field is required.")]
         [Display(Name = "First Name")]
         public string FirstName { get; set; }
-
+        [Required(ErrorMessage = "The Last Name field is required.")]
         public string LastName { get; set; }
-        //[Required]
+        [Required(ErrorMessage = "The Email field is required.")]
         public string Email { get; set; }
-        //[Required]
+        [Required(ErrorMessage = "The Password field is required.")]
         public string Password { get; set; }
-        //[Required]
+       
 
         //[Required]
-        public int AllergyId { get; set; }
-        //public int UserId {  get; set; }
-
+       
 
     }
 }
